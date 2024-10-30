5c27098c70228fe09f89e359d8d2ec7525ef1305
glzzzzzz <135109408+glzzzzzz@users.noreply.github.com.>
2024-10-30 21:06:25 +0100

    start

---
+++ code/main.py
@@ -0,0 +1,30 @@
+from config import *
+from level import Level
+from pytmx.util_pygame import load_pygame
+from os.path import join
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('potato')
+
+        self.tmx_maps = {0: load_pygame('./data/background/test_map.tmx')}
+        
+
+        self.level = Level(self.tmx_maps[0])
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT :
+                    pygame.quit()
+
+            self.level.run()
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
