a146e673817590fc2e1e0204a2e178299c08e11a
glzzzzzz <135109408+glzzzzzz@users.noreply.github.com.>
2025-02-03 15:50:56 +0100

    test

--- code/server.py
+++ code/server.py
@@ -3,7 +3,7 @@ from _thread import *
 import sys
 
 
-server = ""
+server = "192.168.1.123"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,9 +20,29 @@ print("Waiting for a connection, Server Started")
 
 def threaded_client(conn):
     
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = str.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received:", reply)
+                print("Sending:", reply)
+            
+            conn.sendall(str.encode(reply))
+
+        except: 
+            break
+
+
 
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
     start_new_thread(threaded_client, (conn))
+    
